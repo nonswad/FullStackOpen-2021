f874ca5c5206cf824a553970fdc2602f40f551af
nonswad <vedad.spahic@outlook.com>
2021-12-04 00:16:01 +0100

    Phonebook database integrated with frontend, deployed to Heroku

--- part3/phonebook-database/index.js
+++ part3/phonebook-database/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 const Person = require('./models/person')
 
 app.use(express.json())
@@ -11,48 +11,42 @@ app.use(express.static('build'))
 
 var morgan = require('morgan')
 morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
 app.use(morgan('tiny'))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-      }
+  console.error(error.message)
 
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
-app.get('/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date}</p>`
-    )
-})
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
@@ -60,41 +54,41 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    if(!body.name || !body.number)  {
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
+  const body = request.body
 
-    console.log(body.name)
-
-    const person = new Person({
-        name: body.name, 
-        number: body.number,
+  if(!body.name || !body.number)  {
+    return response.status(400).json({
+      error: 'name or number missing'
     })
+  }
 
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  console.log(body.name)
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, {runValidators: true })
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
