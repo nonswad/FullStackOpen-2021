4cdc7fa885fd0c9cd4032dd3101a5db8f9d1ac6c
nonswad <vedad.spahic@outlook.com>
2021-12-11 16:48:46 +0100

    Added user administration and token authetication (4.10 - 4.21)

--- part4/blogList/tests/blogs_api.test.js
+++ part4/blogList/tests/blogs_api.test.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+var util= require('util')
+var encoder = new util.TextEncoder('utf-8')
 const supertest = require('supertest')
 const app = require('../app')
 const Helper = require('./test_helpers.js')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -35,7 +38,7 @@ test('POST request succesfully creates a new blog', async () => {
   await api
     .post('/api/blogs')
     .send(newBlog)
-    .expect(201)
+    .expect(200)
     .expect('Content-Type', /application\/json/)
 
   const blogsAtEnd = await Helper.blogsInDb()
@@ -56,7 +59,7 @@ test('POST request with no likes returns 0 likes', async () => {
   await api
     .post('/api/blogs')
     .send(newBlog)
-    .expect(201)
+    .expect(200)
     .expect('Content-Type', /application\/json/)
 
   const blogsAtEnd = await Helper.blogsInDb()
@@ -65,6 +68,65 @@ test('POST request with no likes returns 0 likes', async () => {
 
   const addedBlog = blogsAtEnd.find(blog => blog.title === 'Zero likes')
   expect(addedBlog.likes).toBe(0)
+})
+
+test('title or url missng returns status code 400', async () => {
+  const newBlog = {
+    author: 'No url',
+    author: 'Noone'
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+test('delete request with valid id is sucessful', async () => {
+  const blogsAtBeginning = await Helper.blogsInDb()
+  const blogToDelete = blogsAtBeginning[1]
+  
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await Helper.blogsInDb()
+
+  expect(blogsAtEnd.length).toBe(blogsAtBeginning.length - 1)
+})
+
+test('delete request with invalid id returns status code 400', async () => {
+  const invalidId = '5a3d5da59070081a82a3445'
+  
+  await api
+    .delete(`/api/blogs/${invalidId}`)
+    .expect(400)
+})
+
+describe('adding invalid user', () => {
+  test('missing usernmame', async () => {
+    const newUser = {
+      name: "testing",
+      password: "123456"
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+  })
+
+  test('invalid password', async () => {
+    const newUser = {
+      username: "test password",
+      name: "testing",
+      password: "12"
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+  })
 })
 
 afterAll(() => {
