a4ae9abfddf5778dbf4c975dc3d164c780b97dc2
nonswad <vedad.spahic@outlook.com>
2021-12-13 00:15:02 +0100

    frontend login added (exercises 5.1 - 5.4)

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const login = JSON.parse(loggedUserJSON)
+      setUser(login)
+      blogService.setToken(login.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+        const login = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem('loggedUser', JSON.stringify(login))
+      blogService.setToken(login.token)
+      setUser(login)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {      
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    setUser(null)
+    setUsername('')
+    setPassword('')
+    blogService.setToken(null)
+    window.localStorage.clear()
+  }
+
+  const addNewBlog = async (event) => {
+    event.preventDefault()
+
+    const newObject = {
+      title: title,
+      author: author,
+      url: url,
+      likes: 0
+    }
+
+    try{
+      await blogService
+      .create(newObject)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setMessage(`a new blog ${newObject.title}, by ${newObject.author} added`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+    catch(exception) {
+      console.log('exception is', exception)
+    }
+
+  }
+
+  const loginForm = () => (
+    <form onSubmit = {handleLogin}>
+      <div>
+      <h2>log in to application</h2>
+      <h2>{message}</h2>
+        username
+        <input 
+          type = "text"
+          value = {username}
+          name = "Username"
+          onChange = {({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange = {({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <div>
+      <h2>blogs</h2>
+      <h2>{message}</h2>
+      <p>
+        {username} logged in
+        <button onClick = {handleLogout}>logout</button>
+      </p>
+
+      <h2>create new</h2>
+      <form onSubmit = {addNewBlog}> 
+      <div>
+          title
+          <input
+            type = "text"
+            value = {title}
+            name = "Title"
+            onChange = {({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+        author
+          <input
+            type = "text"
+            value = {author}
+            name = "Author"
+            onChange = {({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+        url
+          <input
+            type = "text"
+            value = {url}
+            name = "Url"
+            onChange = {({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null && loginForm()}
+      {user !== null && blogForm()}
+    </div>
+  )
+}
+
+export default App
